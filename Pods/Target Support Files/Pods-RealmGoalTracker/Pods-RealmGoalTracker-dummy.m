1648513104ea714aa6832f469dbfa911b3a3a562
JoesMac <jantongiovanni@csumb.edu>
2018-08-17 14:34:50 -0700

    first commit

---
+++ Pods/Target Support Files/Pods-RealmGoalTracker/Pods-RealmGoalTracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmGoalTracker : NSObject
+@end
+@implementation PodsDummy_Pods_RealmGoalTracker
+@end
---
+++ RealmGoalTracker/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  RealmGoalTracker
+//
+//  Created by JoesMac on 8/17/18.
+//  Copyright © 2018 JoeA. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
