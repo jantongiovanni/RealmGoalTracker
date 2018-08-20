3e4b277252b6f7d3e305a1a48c5341c4677e8c6a
JoesMac <jantongiovanni@csumb.edu>
2018-08-20 16:09:26 -0700

    ToDo tab setup

--- RealmGoalTracker/FirstViewController.swift
+++ RealmGoalTracker/FirstViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,24 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
 
+extension FirstViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "GoalCell") as? GoalCell else { return UITableViewCell() }
+        //let pickUpLine = pickUpLines[indexPath.row]
+        //cell.configure(with: pickUpLine)
+       return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 63
+    }
+}
+
+extension FirstViewController: UITableViewDelegate {
+}
---
+++ RealmGoalTracker/GoalCell.swift
@@ -0,0 +1,15 @@
+//
+//  GoalCell.swift
+//  RealmGoalTracker
+//
+//  Created by JoesMac on 8/20/18.
+//  Copyright © 2018 JoeA. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalLabel: UILabel!
+    
+}
